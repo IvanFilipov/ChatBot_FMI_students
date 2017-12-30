87480d434e2c03dc5c48ed289c5b66ad9c55cea5
IvanFilipov <vanaka1189@gmail.com>
2017-12-30 17:32:32 +0200

    first keyboard menu

--- chat_bot/src/bot.js
+++ chat_bot/src/bot.js
@@ -4,6 +4,7 @@ console.log('hi, i am the chatbot :)');
 
 //console.log(process.argv);
 
+//the token passed as argument
 let botToken;
 
 function init(){
@@ -26,11 +27,33 @@ catch(err){
     process.exit(-1);
 }
 
+//creating the bot context
 const bot = new TelegramBot(botToken, {polling: true});
+
+//first message ever
+bot.onText(/\/start/, (msg) => {
+    
+    let answer = "Welcome, " + msg.chat.first_name + ' ' + msg.chat.last_name + '!';
+
+    bot.sendMessage(msg.chat.id, answer, { 
+        
+        "reply_markup": {
+
+            "keyboard": [
+                ["News"],
+                ["Events", "Personal info"],
+                ["General information"]
+            ]
+        }
+
+    });
+
+});
+
 
 bot.on('message',(msg) =>{
 
-    bot.sendMessage(msg.chat.id, msg.text);
+    bot.sendMessage(msg.chat.id, msg.text + " pressed!");
 
 });
 
