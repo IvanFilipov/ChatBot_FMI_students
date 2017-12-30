2387221e0e7b7dfdc75f57d0d28d3f5847cf68b8
IvanFilipov <vanaka1189@gmail.com>
2017-12-30 15:10:02 +0200

    echo bot

--- chat_bot/src/bot.js
+++ chat_bot/src/bot.js
@@ -1,1 +1,37 @@
+const TelegramBot = require('node-telegram-bot-api');
+
 console.log('hi, i am the chatbot :)');
+
+//console.log(process.argv);
+
+let botToken;
+
+function init(){
+
+    botToken = process.argv[2];
+
+    if(botToken === undefined)
+        throw Error("can't process");
+
+}
+
+try {
+
+    init();
+    console.log(botToken);
+}
+catch(err){
+
+    console.error(err);
+    process.exit(-1);
+}
+
+const bot = new TelegramBot(botToken, {polling: true});
+
+bot.on('message',(msg) =>{
+
+    bot.sendMessage(msg.chat.id, msg.text);
+
+});
+
+
