c97961a414f3fcbc5d7e3762acce4986e2ef2a1b
IvanFilipov <vanaka1189@gmail.com>
2017-12-31 17:14:54 +0200

    handle invalid input

--- chat_bot/src/bot.js
+++ chat_bot/src/bot.js
@@ -2,6 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 
 console.log('hi, i am the chatbot :)');
 
+//will be used to remember language settings
+const usersStates = {};
+
 //console.log(process.argv);
 
 //the token passed as argument
@@ -30,30 +33,42 @@ catch(err){
 //creating the bot context
 const bot = new TelegramBot(botToken, {polling: true});
 
+
+const optionsInEnglish = { 
+    
+    "reply_markup": {
+
+        "keyboard": [
+            ["News"],
+            ["Events", "Personal info"],
+            ["General information"],
+            ["Test my knowledge"]
+        ]
+    }
+
+};
+
 //first message ever
 bot.onText(/\/start/, (msg) => {
     
-    let answer = "Welcome, " + msg.chat.first_name + ' ' + msg.chat.last_name + '!';
+    let answer = "Welcome, " + msg.chat.first_name + ' '
+                             + msg.chat.last_name + '!';
 
-    bot.sendMessage(msg.chat.id, answer, { 
-        
-        "reply_markup": {
-
-            "keyboard": [
-                ["News"],
-                ["Events", "Personal info"],
-                ["General information"]
-            ]
-        }
-
-    });
+    bot.sendMessage(msg.chat.id, answer, optionsInEnglish);
 
 });
 
-
 bot.on('message',(msg) =>{
 
-    bot.sendMessage(msg.chat.id, msg.text + " pressed!");
+    const optIndex = optionsInEnglish.reply_markup.keyboard.
+                                                    join(' ').
+                                                    indexOf(msg.text);
+    
+    //no such option
+    if(optIndex === -1)
+        bot.sendMessage(msg.chat.id, "I don't understand you 😓");
+    else
+        bot.sendMessage(msg.chat.id, msg.text + " pressed!");
 
 });
 
